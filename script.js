8647ff1ff7ea4eea4c7cea251f1976e9dc99fa13
Guilherme Sousa <guilhermefeijodesousa@gmail.com>
2025-04-15 14:41:04 -0300

    adicionando todos os arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+let botao = document.querySelector("#botao");
+
+
+botao.addEventListener ('click', trocatexto);
+function trocatexto(){
+    botao.innerHTML = "Feliz aniversário, meu amor! 💖Te desejo todas as melhores coisas que a vida pode oferecer: muita saúde, amor, conquistas e felicidade em cada passo do teu caminho.Sou imensamente feliz por estar ao teu lado, dividindo momentos, sonhos e sorrisos.TE AMO 🥰🎉";
+    botao.style.background = "#ff0042";   
+}
+
+
+
+
+   
+
